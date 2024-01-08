065e4f18be2654386e3a816e6f45766eba417b33
aleff <aleff_diniz@hotmail.com>
2024-01-08 14:41:58 -0300

    implementando formulario de pesquisa

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,15 +1,19 @@
 import { WeatherDatas } from 'src/app/models/interfaces/Weather';
 import { WeatherService } from './../../services/weather.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, take, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent implements OnInit{
+export class WeatherHomeComponent implements OnInit, OnDestroy{
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName ='São Paulo';
   weatherDatas!:WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService:WeatherService){}
 
@@ -17,14 +21,25 @@ export class WeatherHomeComponent implements OnInit{
     this.getWheatherDatas(this.initialCityName);
   }
     getWheatherDatas(cityName:string):void{
-      this.weatherService.getWeatherDatas(cityName).subscribe({
+      this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
         next: (response) => {
           //console.log(response);
-          //response && (this.weatherDatas = response);
-          //console.log(this.weatherDatas.main.)
-
+          response && (this.weatherDatas = response);
+          console.log(this.weatherDatas)
         },
         error:(error) => console.log(error),
-      })
-    }
+      });
   }
+
+    onsubmit():void {
+      this.getWheatherDatas(this.initialCityName);
+      this.initialCityName='';
+    }
+    ngOnDestroy(): void {
+      this.destroy$.next();
+      this.destroy$.complete();
+    }
+}
