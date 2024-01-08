b2e6aef20fb4f99a04762ecbe9251fedd8ce7364
aleff <aleff_diniz@hotmail.com>
2024-01-08 12:29:30 -0300

    consumindo serviço e adicionando imagens no assets

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/Weather';
+import { WeatherService } from './../../services/weather.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit{
+  initialCityName ='São Paulo';
+  weatherDatas!:WeatherDatas;
 
-}
+  constructor(private weatherService:WeatherService){}
+
+  ngOnInit(): void {
+    this.getWheatherDatas(this.initialCityName);
+  }
+    getWheatherDatas(cityName:string):void{
+      this.weatherService.getWeatherDatas(cityName).subscribe({
+        next: (response) => {
+          //console.log(response);
+          //response && (this.weatherDatas = response);
+          //console.log(this.weatherDatas.main.)
+
+        },
+        error:(error) => console.log(error),
+      })
+    }
+  }
